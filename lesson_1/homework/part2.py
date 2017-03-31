ed409127830c706d7fed9aa48abb72ef55a77a07
tanyagreen <kaktusgreen.kg@gmail.com>
2017-03-31 16:59:29 +0300

    + part3.py 3-я часть 3-го задания

--- lesson_1/homework/part2.py
+++ lesson_1/homework/part2.py
@@ -2,34 +2,52 @@
 # -*- coding: utf-8 -*-
 
 import hashlib
+import os
+import pickle
 
-def get_hash(string, mode):
+__author__ = "Tatiana Karavaeva"
+
+def get_hash_sum(string, mode):
     """На входе: строка, название алгоритма хэш-суммы
-	На выходе: hex-представление хэш-суммы для строки
+	На выходе: hex-представление хэш-суммы 
     """
+    
+    dct_modes = dict(
+        sha1 = hashlib.sha1,
+        md5 = hashlib.md5,
+        sha512 = hashlib.sha512
+        )
+
     s = string.encode('utf8')
 
-    if mode == 'md5':
-        h = hashlib.md5()
-    elif mode == 'sha1':
-        h = hashlib.sha1()
-    elif mode == 'sha512':
-        h = hashlib.sha512()
-
+    h = dct_modes[mode]()
     h.update(s)
     return h.hexdigest()
 
+def main():
+    """
+    Читает need_hashes.csv, добывает хешсуммы для каждой строки в файле
+    дописывает полученную хешсумму обратно в файл 
+    """
+    
+    path = 'need_hashes.csv'
+    line_new = []
+    with open(path, 'r', encoding='UTF-8') as f:
+        for i, line in enumerate(f):
+            l = line.split(';')
+            hash_sum = get_hash_sum(l[0], l[1])
+            if i > 0:
+                 line_new.insert(i,'{};{};{}{}'.format(l[0], l[1], hash_sum, '\n'))
+            else:
+                 line_new.insert(i,line)
 
-path = 'need_hashes.csv'
-line_new = []
-with open(path, 'r', encoding='UTF-8') as f:
-    for i, line in enumerate(f):
-        l = line.split(';')
-        hash_sum = get_hash(l[0], l[1])
-        if i > 0:
-            line_new.insert(i,'{};{};{}{}'.format(l[0], l[1], hash_sum, '\n'))
-        else:
-            line_new.insert(i,line)
+    with open(path, 'w', encoding='UTF-8') as f:
+        f.writelines(line_new)
 
-with open(path, 'w', encoding='UTF-8') as f:
-    f.writelines(line_new)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
--- lesson_1/homework/part3.py
+++ lesson_1/homework/part3.py
@@ -1,9 +1,73 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import hashlib
 
-path = 'files/file1/parts.md5'
-line_new = []
-with open(path, 'r', encoding='UTF-8') as f:
-    for i, line in enumerate(f):
-       print(line)
+
+def get_hash_sum(byte, mode):
+    """На входе: посдедовательность байт, название алгоритма хэш-суммы
+	На выходе: hex-представление хэш-суммы 
+    """
+    
+    dct_modes = dict(
+        sha1 = hashlib.sha1,
+        md5 = hashlib.md5,
+        sha512 = hashlib.sha512
+        )
+
+    h = dct_modes[mode]()
+    h.update(byte)
+    return h.hexdigest()
+
+
+def glue_file(dir_name, hash_file_name, res_file_name):
+    """
+    функцию "склеивания" файла на основе упорядоченных хэш-сумм
+
+    На входе: имя директории с файлами-кусочками, имя файла с хэш-суммами, имя выходного файла
+    На выходе: размер полученного файла 
+    """
+
+    DIR = os.path.join('files', dir_name)
+    
+    with open(os.path.join(DIR, hash_file_name), 'r', encoding='UTF-8') as f:
+        lst_hashes = [line.replace('\n', '') for line in f]
+
+    list_files = [file_name for file_name in os.listdir(DIR)
+              if not file_name == hash_file_name]
+    res_lst = []
+    
+    for file in list_files:
+        with open(os.path.join(DIR, file), 'rb') as f:
+            read_data = f.read()
+            h_summ = get_hash_sum(read_data, 'md5')
+   
+        for i, item in enumerate(lst_hashes):
+            if h_summ == item:
+                print('{} + {}'.format(h_summ, i))
+                res_lst.insert(i, file)
+
+    with open(os.path.join('files', res_file_name), 'wb') as f_res:
+        for file in res_lst:
+            with open(os.path.join(DIR, file), 'rb') as f:
+                read_data = f.read()
+            f_res.write(read_data)
+            
+    return os.path.getsize(os.path.join('files', res_file_name))
+
+
+def main():   
+    """
+    main func 
+    """
+
+    print(glue_file('file2','parts.md5', 'resss2'))
+
+
+if __name__ == '__main__':
+    main()
+
+ 
+
+
